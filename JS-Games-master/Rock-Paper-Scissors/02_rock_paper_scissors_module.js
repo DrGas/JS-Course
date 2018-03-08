16acc429bf45e8c4eb0676c32ad34c6e81540658
DrGas <magdalena.drgas@gmail.com>
2018-03-08 12:06:05 +0100

    Dice Game

--- JS-Games-master/Rock-Paper-Scissors/02_rock_paper_scissors_module.js
+++ JS-Games-master/Rock-Paper-Scissors/02_rock_paper_scissors_module.js
@@ -2,9 +2,15 @@ var myGame = (function () {
 
 	// private variables
 	var myVar = "some value or expression";
+	
+	var privateHumanScore = 0;
+	var privateComputerScore = 0;
 
 	// functions
 	var myPrivateFunction;
+	var myPrivatecomputerInput;
+	var compare;
+	
 	
 
 	// private functions
@@ -12,10 +18,54 @@ var myGame = (function () {
 	
 	};
 	
+	
+	myPrivatecomputerInput = function() {
+
+			switch (Math.floor(Math.random() * 3)) //return a whole number between 0-2 
+			{
+				case 0:
+					this.computerChoice = "Rock"; //using this! not using the variable but changing poperties 
+					break;
+
+				case 1:
+					this.computerChoice = "Paper";
+					break;
+
+				case 2:
+					this.computerChoice = "Scissors";
+					break;
+			}
+
+	
 	return {
 		// public functions
-		myPublicFunction: function() {
-			
+		compare: function() {
+			if (userChoice === computerChoice) {
+		resultDisplay.innerHTML = 'Game is a draw';
+	}
+	//all other possibilities 
+	else if (userChoice === 'Paper' && computerChoice ==='Rock' ) {
+				resultDisplay.innerHTML = 'You win! Wohooo!!';
+	}
+	
+	else if (userChoice === 'Paper' && computerChoice ==='Scissors' ) {
+				resultDisplay.innerHTML = 'You loose! Buuuu!';
+	}
+	
+	else if (userChoice === 'Scissors' && computerChoice ==='Rock' ) {
+				resultDisplay.innerHTML = 'You loose! Buuuu!';
+	}
+	
+	else if (userChoice === 'Scissors' && computerChoice ==='Paper' ) {
+				resultDisplay.innerHTML = 'You win! Wohooo!!';
+	}
+	
+	else if (userChoice === 'Rock' && computerChoice ==='Paper' ) {
+				resultDisplay.innerHTML = 'You loose! Buuuu!';
+	}
+	else if (userChoice === 'Rock' && computerChoice ==='Scissors' ) {
+				resultDisplay.innerHTML = 'You win! Wohooo!!';
+	}
 		}
 	};
 
---
+++ dices_game/dices.js
@@ -0,0 +1,151 @@
+function humanName() {
+	'use strict';
+    var human = prompt("Please enter your name", " ");
+    if (human !== null) {
+        document.getElementById("human").innerHTML =
+        "Hello " + human;
+    }
+}
+humanName();
+
+
+
+
+var dice1 = 0;
+var dice2 = 0;
+var resultComputer;
+var resultHuman;
+var newGame = document.getElementById("playAgain");
+var scoreComputer = 0;
+var scoreHuman = 0;
+
+var dice1Computer = document.getElementById("dice1computer");
+var dice2Computer = document.getElementById("dice2computer");
+
+var dice1Human = document.getElementById("dice1human");
+var dice2Human = document.getElementById("dice2human");
+
+
+var resultComputerDisplay = document.getElementById("resultComputer");
+var resultHumanDisplay = document.getElementById("resultHuman");
+
+var computerPoints = document.getElementById("computerPoints");
+var humanPoints = document.getElementById("humanPoints");
+ 
+
+
+var finalScoreDisplay = document.getElementById("whoWins");
+
+
+
+class Dice {
+	constructor(dice1, dice2) {
+		this.dice1 = dice1;
+		this.dice2 = dice2;
+	}
+
+	rollDices() {
+		dice1 = (Math.floor(Math.random() * 6) + 1);
+		dice2 = (Math.floor(Math.random() * 6) + 1);
+		//console.log("Computer rolled: " + dice1);
+		//console.log("Computer rolled: " + dice2);
+		dice1Computer.innerHTML = "Dice 1: " + dice1;
+		dice2Computer.innerHTML = "Dice 2: " + dice2;
+
+
+	}
+
+	calculate() {
+		resultComputer = dice1 + dice2;
+		//console.log("Computer score: " + resultComputer);
+		resultComputerDisplay.innerHTML = "Computer score: " + resultComputer;
+
+	}
+}
+
+
+var MyDice = new Dice();
+MyDice.rollDices();
+MyDice.calculate();
+
+
+class Player extends Dice {
+
+	constructor() {
+		super(); //inherits the constructor 
+	}
+
+	rollDices() {
+		dice1 = (Math.floor(Math.random() * 6) + 1);
+		dice2 = (Math.floor(Math.random() * 6) + 1);
+		//console.log("You rolled: " + dice1);
+		//console.log("You rolled: " + dice2);
+		dice1Human.innerHTML = "Dice 1: " + dice1;
+		dice2Human.innerHTML = "Dice 2: " + dice2;
+	}
+
+	calculate() {
+		resultHuman = dice1 + dice2;
+		//console.log("Your score: " + resultHuman);
+		resultHumanDisplay.innerHTML = "Your score: " + resultHuman;
+
+	}
+
+}
+
+var MyPlayer = new Player();
+MyPlayer.rollDices();
+MyPlayer.calculate();
+
+function compare() {
+	'use strict';
+	if (resultComputer === resultHuman) {
+		console.log("It's a draw");
+		finalScoreDisplay.innerHTML = "It's a draw";
+	} else if (resultComputer > resultHuman) {
+		scoreComputer++;
+		//console.log("You loose :(");
+		finalScoreDisplay.innerHTML = "You loose :(";
+		//console.log("Computer score: " + scoreComputer + " Human score: " + scoreHuman ); 
+		computerPoints.innerHTML = "Computer points: " + scoreComputer;
+	} else if (resultComputer < resultHuman) {
+		scoreHuman++;
+		//console.log("Very much a winner YES!!");
+		finalScoreDisplay.innerHTML = "Very much a winner YES!!";
+		//console.log("Human score: " + scoreHuman + " Computer score: " + scoreComputer);
+		humanPoints.innerHTML = "Your points: " + scoreHuman;
+	}
+}
+
+compare();
+
+
+function NewGame() {
+	'use strict';
+
+	var MyDice = new Dice();
+	MyDice.rollDices();
+	MyDice.calculate();
+
+	var MyPlayer = new Player();
+	MyPlayer.rollDices();
+	MyPlayer.calculate();
+
+	compare();
+}
+
+newGame.addEventListener('click', NewGame);
+
+
+
+//var NewDice = new Dice();
+//	NewDice.rollDices();
+//	NewDice.calculate();
+//	
+//	var NewPlayer = new Player();
+//	NewPlayer.rollDices();
+//	NewPlayer.calculate();	
+//	
+//	NewDice.addEventListener('click', PlayAgain);
+//	NewPlayer.addEventListener('click', PlayAgain);
+//	newGame.addEventListener('click', PlayAgain);
